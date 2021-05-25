f2ab1ce2a75721fc790cc350089c945f0ce0a47b
gespro <utest03>
2021-05-25 13:28:42 +0200

    Initial Commit

---
+++ Shared/GesproApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesproApp.swift
+//  Shared
+//
+//  Created by Manuel Gonzalez Blanco on 25/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GesproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
